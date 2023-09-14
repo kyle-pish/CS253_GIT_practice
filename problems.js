6db5776a4e7ad67a968c8e1553e177b7aa28ac90
Justin Bruso <jbruso@iwu.edu>
2023-09-14 10:00:14 -0500

    Adding the Day

--- problems.js
+++ problems.js
@@ -1,1 +1,6 @@
-
+var today = new Date();
+var day = today.getDay();
+var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+console.log("Today is " + dayList[day] + ".")
+var hour = today.getHours();
+var minute = today.getMinutes();
